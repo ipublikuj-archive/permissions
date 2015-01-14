55ecd8e870ac1f04e3d7f7f4282ccdc23ee576a1
Adam Kadlec <adam.kadlec@fastybird.com>
2015-01-14 21:49:37 +0100

    - Added travis settings

---
+++ .travis.composer.php
@@ -0,0 +1,19 @@
+<?php
+
+$version = getenv('NETTE');
+
+if (!$version || $version == 'default') {
+	exit;
+}
+
+echo "Nette version " . $version . PHP_EOL;
+
+$file = __DIR__ . '/composer.json';
+$content = file_get_contents($file);
+$composer  = json_decode($content, TRUE);
+
+$composer['require']['nette/application'] = $version;
+$composer['require']['nette/di'] = $version;
+$composer['require']['nette/utils'] = $version;
+$content = json_encode($composer);
+file_put_contents($file, $content);
