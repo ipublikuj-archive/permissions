6c3219732ba1514fba96ff83a7eff2f9c720c268
Adam Kadlec <adam.kadlec@fastybird.com>
2017-01-02 22:13:26 +0100

    - Updating tests settings

--- tests/IPubTests/bootstrap.php
+++ tests/IPubTests/bootstrap.php
@@ -10,11 +10,17 @@ if (!class_exists('Tester\Assert')) {
 }
 
 Tester\Environment::setup();
+date_default_timezone_set('Europe/Prague');
 
 // Create temporary directory
 define('TEMP_DIR', __DIR__ . DS . '..' . DS . 'tmp' . DS . (isset($_SERVER['argv']) ? md5(serialize($_SERVER['argv'])) : getmypid()));
 Tester\Helpers::purge(TEMP_DIR);
 \Tracy\Debugger::$logDirectory = TEMP_DIR;
+
+$_SERVER = array_intersect_key($_SERVER, array_flip(array(
+	'PHP_SELF', 'SCRIPT_NAME', 'SERVER_ADDR', 'SERVER_SOFTWARE', 'HTTP_HOST', 'DOCUMENT_ROOT', 'OS', 'argc', 'argv')));
+$_SERVER['REQUEST_TIME'] = 1234567890;
+$_ENV = $_GET = $_POST = array();
 
 function id($val)
 {
